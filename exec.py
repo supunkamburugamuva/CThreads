3f6711616646a514a76a7226022947ba14822bd3
Supun <supun06@gmail.com>
2015-12-14 02:26:15 -0500

    fixing

--- exec.py
+++ exec.py
@@ -5,16 +5,17 @@ import subprocess
 def exec_main():
     iter = [100]
     rows = [87, 174, 348, 696, 1392, 2088]
-    cols = [2000, 1000, 4000]
+    cols = [200000, 100000, 400000]
     threads = [1, 2, 4, 8, 16, 24]
 
     for i in iter:
-        for r in rows:
-            for c in cols:
-                for t in threads:
-                    p = subprocess.Popen(['./a.out', str(t), str(i), str(r * c / 1000), str(c)], stdout=subprocess.PIPE);
-                    out, err = p.communicate()
-                    print out + "\n"
+        for c in cols:
+            r_in = 87 * c / 100000
+            for t in threads:
+                r = r_in * t
+                p = subprocess.Popen(['./a.out', str(t), str(i), str(r), str(c)], stdout=subprocess.PIPE);
+                out, err = p.communicate()
+                print out
 
 if __name__ == "__main__":
     exec_main()
